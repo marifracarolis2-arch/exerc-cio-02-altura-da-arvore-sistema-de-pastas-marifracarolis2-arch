2b81cb58a2a4568cb604896952556392f5d0d398
MARIANA FRACAROLI LOPES <mariana.flopes2@senacsp.edu.br>
2026-05-26 10:51:42 -0300

    Resolução dos exercícios

---
+++ exercicios/Ex01AlturaDaArvore.java
@@ -0,0 +1,52 @@
+package exercicios;
+
+public class Ex01AlturaDaArvore {
+
+    static class No {
+
+        String nome;
+        No left;
+        No right;
+
+        No(String nome){
+            this.nome = nome;
+        }
+    }
+
+    public static int altura(No raiz){
+        if (raiz == null){
+            return 0;
+        }
+        int alturaEsquerda = altura(raiz.left);
+        int alturaDireita = altura(raiz.right);
+
+        int maiorAltura = Math.max(alturaEsquerda, alturaDireita);
+
+        return 1 + maiorAltura;
+        //return 1 + Math.max(alturaEsquerda, alturaDireita);
+    }
+
+    public static No pastasEmpresa(){
+        No raiz = new No("root");
+        raiz.left = new No("home");
+        raiz.right = new No("user");
+        raiz.left.left = new No("documents");
+        raiz.right.right = new No("downloads");
+
+        return raiz;
+
+
+    }
+
+     public static void main(String[] args) {
+
+        No raiz = pastasEmpresa();
+
+        //arvore pequena
+        System.out.println("Altura da árvore: " + altura(raiz));
+
+        //arvore vazia
+        System.out.println("Altura árvore vazia: " + altura(null));
+    }  
+}
+
